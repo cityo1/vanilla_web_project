ae23ff56e95edc43bf77034b8524154a49c32db1
cityo1 <ansrnrgus1@naver.com>
2026-01-15 19:49:15 +0900

    feature section complete

--- js/custom.js
+++ js/custom.js
@@ -1,22 +1,23 @@
-/** Header Change Effect **/
-const header = document.querySelector('#header');
-
-const stickyHeader = () => {
-  const scry = window.scrollY;
-  if (scry > 0) {
-    header.classList.add('active');
-  } else header.classList.remove('active');
-};
-
-window.addEventListener('scroll', stickyHeader);
-
 /** Scroll Reveal Effect **/
 const sr = ScrollReveal({
-  reset: false, // 스크롤 시 한번만 실행
+  reset: true, // 스크롤 시 한번만 실행(false), 계속 실행(true)
 });
 
 sr.reveal('.landing-text-box', {
   duration: 1000,
   origin: 'right',
   distance: '80px',
+});
+
+sr.reveal('.meet-text-box', {
+  duration: 1000,
+  origin: 'bottom',
+  distance: '40px',
+});
+
+sr.reveal('.meet-wrapper img, .feature', {
+  duration: 1000,
+  origin: 'bottom',
+  distance: '40px',
+  inteval: 200, // 0.2초 간격으로 차례로 효과 진행
 });
