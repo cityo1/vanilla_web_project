4d77b157bc432aa1f9ece0a971fa16fbf1f9550b
cityo1 <ansrnrgus1@naver.com>
2026-01-19 15:58:50 +0900

    review section complete

--- js/request.js
+++ js/request.js
@@ -1,63 +1,64 @@
 async function getRequest(url) {
   return await fetch(url).then((response) => {
     if (!response.ok) {
-      throw new Error('Error:' + response.status);
+      throw new Error('Error: ' + response.status);
     }
     return response.json();
   });
+}
+
+function createSliderTemplate(item) {
+  const { pro_img, pro_name, pro_desc } = item;
+  return `
+    <div class="swiper-slide">
+      <div class="slider-image">
+        <img src="images/${pro_img}" alt="slider image">
+      </div>
+      <div class="slider-text">
+        <h4>${pro_name}</h4>
+        <p>${pro_desc}</p>
+        <a href="#" class="common-button">자세히 보기</a>
+      </div>
+    </div>
+  `;
+}
+
+function createOfferTemplate(item) {
+  const { pro_img, pro_name, pro_desc, pro_price } = item;
+  return `
+    <div class="product-frame">
+      <div class="product-item">
+        <div class="product-img">
+          <img src="images/${pro_img}" alt="item image">
+        </div>
+        <div class="product-text">
+          <h4>${pro_name}</h4>
+          <strong>${pro_price}원</strong>
+          <p>${pro_desc}</p>
+          <a href="#" class="common-button">자세히 보기</a>
+        </div>
+      </div>
+    </div>
+  `;
 }
 
 // Get Products Function
 const sliderWrapper = document.querySelector('.swiper-wrapper');
 const offersWrapper = document.querySelector('.products');
 
-// const sliderDOM = `
-//               <div class="swiper-slide">
-//                 <div class="slider-image">
-//                   <img src="images/${pro_img}" alt="slider image" />
-//                 </div>
-//                 <div class="slider-text">
-//                   <h4>
-//                     ${pro_name}
-//                   </h4>
-//                   <p>
-//                     ${pro_desc}
-//                   </p>
-//                   <a href="#" class="common-button">자세히 보기</a>
-//                 </div>
-//               </div>
-//     `;
-
-async function getProducts(n) {
+async function getProducts(n, wrapper, template) {
   const getProductsUrl = `https://www.dabipyeung.com/soaply_backend/model/get_products.php?qnt=${n}`;
 
   try {
+    // 요청 시도
     const data = await getRequest(getProductsUrl);
-    let dataElement = '';
-    data.map((item) => {
-      const { pro_img, pro_name, pro_desc } = item;
-      dataElement += `
-              <div class="swiper-slide">
-                <div class="slider-image">
-                  <img src="images/${pro_img}" alt="slider image" />
-                </div>
-                <div class="slider-text">
-                  <h4>
-                    ${pro_name}
-                  </h4>
-                  <p>
-                    ${pro_desc}
-                  </p>
-                  <a href="#" class="common-button">자세히 보기</a>
-                </div>
-              </div>
-    `;
-    });
-
-    sliderWrapper.insertAdjacentHTML('beforeend', dataElement);
+    const htmlString = data.map(template).join('');
+    wrapper.insertAdjacentHTML('beforeend', htmlString);
   } catch (error) {
     // 요청 시 에러 사항
     console.log(`Error: ${error}`);
   }
 }
-getProducts(4);
+
+getProducts(4, sliderWrapper, createSliderTemplate);
+getProducts(3, offersWrapper, createOfferTemplate);
